458ba5d57ae61b2ac3e583c27593977f5ab28e6e
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-18 18:56:37 -0700

    Pass callback review tests

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,12 +6,28 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function (err, contents) {
+    if (err) {
+      callback(err);
+    } else {
+      var firstLine = contents.split('\n');
+      callback(null, firstLine[0]);
+    }  
+  });
   // TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
+  request.get(url, function(err, res, body) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, res.statusCode);
+    }
+
+  });
   // TODO
 };
 
